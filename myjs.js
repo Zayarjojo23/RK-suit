c5b31899419f282ffd0f7fdc9bbf5518cd7d4d61
Zayarjojo23 <141992239+Zayarjojo23@users.noreply.github.com>
2023-08-16 12:16:40 +0630

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -2,6 +2,7 @@ function addquality(){
     let quality = document.getElementById('quality').value;
     let priceDisplay =document.getElementById('priceDisplay');
     totalPrice = price * quality;
-    priceDisplay.innerHTML = " Price : $"+totalPrice;
     localStorage.setItem('priceDisplay', totalPrice);
+    priceDisplay.innerHTML = " Price : $"+totalPrice;
+    
 }
